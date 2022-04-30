d0cd02c51e362defd49c507cb10128f72916a43c
Vasilis Milionis <v_milionis@yahoo.com>
2022-04-30 20:25:12 +0300

    Approximating a Wiener filter.
    Method1: Using a constant k
    Method2: Use blocking in the spatial domain in order to approximate the
    SNR of the area, using mean/std approximation.

---
+++ imageRestoration-deconvolution/calcBlindWieners_k.m
@@ -0,0 +1,32 @@
+function [k] = calcBlindWieners_k(inpImg, rows, cols)
+    [M, N] = size(inpImg);
+    % Add the appropriate zero padding to the image, so the kernel may act
+    % upon each edge pixel.
+    if(mod(M,rows))
+        reqRows = ceil(M/rows)*rows - M;
+        usedRows = rows - reqRows;
+        start = M-usedRows-reqRows;
+        inpImg = [inpImg; inpImg(start+1:start+reqRows,:)];
+    end
+    if(mod(N,cols))
+        reqCols = ceil(N/cols)*cols - N;
+        usedCols = cols - reqCols;
+        start = N-usedCols- reqCols;
+        inpImg = [inpImg inpImg(:,start+1:start+reqCols)];
+    end
+    % Iterate through all the pixels that contain true information, find
+    % the DFT
+    [P, Q] = size(inpImg);
+    k = zeros(P,Q);
+    for x=1:rows:P
+        for y=1:cols:Q
+            % Approximate SNR - ?in the spatial domain?
+            block = inpImg(x:x+rows-1,y:y+rows-1);
+            signal = mean(block,'all');
+            noise = std(block,0,'all');
+            k(x:x+rows-1,y:y+rows-1) = noise/signal;
+        end
+    end
+    k = k(1:M,1:N);
+end
+
--- imageRestoration-deconvolution/main.m
+++ imageRestoration-deconvolution/main.m
@@ -4,7 +4,8 @@ clc;
 
 lenna = imread("../Images/exercise5/lenna.jpg");
 
-snr_db = 10;
+snr_db = 10; % SNR in db for AWGN
+k = 0.11; % The constant with which I will approximate the Wiener filter.
 [M, N, d] = size(lenna);
 
 % Again I notice all three channels have the same values so I may work
@@ -12,7 +13,7 @@ snr_db = 10;
 lenna_r = lenna(:,:,1);
 
 % Add zero padding
-dlenna_r = zeros(2*M, 2*N);
+% dlenna_r = zeros(2*M, 2*N);
 % Fix pixel intensities in range [0,1], required for imnoise().
 dlenna_r(1:M,1:N) = im2double(lenna_r);
 
@@ -24,23 +25,43 @@ LENNA_GAUSS = fftshift(fft2(lenna_gauss));
 imshow(log(abs(LENNA_GAUSS)), []);
 
 % Calculate power spectrums
-S_n = fftshift(fft2(dlenna_r - lenna_gauss)).^2;
-S_f = fftshift(fft2(dlenna_r)).^2;
+S_n = abs(fftshift(fft2(dlenna_r - lenna_gauss))).^2;
+S_f = abs(fftshift(fft2(dlenna_r))).^2;
 
-% Construct the smoothing filter Wiener
+% Construct the smoothing filter Wiener - using the knowledge of S_n
 SMOOTH_WIENER = S_f./(S_f+S_n);
 % Apply the filter
 LENNA_SMOOTH = SMOOTH_WIENER.*LENNA_GAUSS;
 
-% Get back to the spatial domain
-lenna_smooth = real(ifft2(ifftshift(LENNA_SMOOTH)));
-imshow(lenna_smooth(1:M,1:N), []);
+% Construct the filter without using S_n
+% k = 3;
+% k = calcBlindWieners_k(lenna_gauss,k,k);
+SMOOTH_WIENER_k = 1./(1+k);
+LENNA_SMOOTH_k = SMOOTH_WIENER_k.*LENNA_GAUSS;
 
-% Plot the original and the noisy image
+% Get back to the spatial domain
+lenna_smooth = ifft2(ifftshift(LENNA_SMOOTH));
+lenna_smooth = lenna_smooth(1:M, 1:N);
+
+lenna_smooth_k = real(ifft2(ifftshift(LENNA_SMOOTH_k)));
+% lenna_smooth_k = wiener2(lenna_gauss, [5 5], variance);
+lenna_smooth_k = lenna_smooth_k(1:M, 1:N);
+
+% Plot
 figure;
-subplot(1,2,1);
+subplot(2,2,1);
 imshow(lenna_r, []);
 title("Original Image");
-subplot(1,2,2);
-imshow(lenna_gauss, []);
+subplot(2,2,2);
+imshow(lenna_gauss(1:M,1:N), []);
 title(strcat("AWGN of ", num2str(snr_db), "db"));
+subplot(2,2,3);
+imshow(lenna_smooth, []);
+title("Using Wiener with knowledge to the power of the noise");
+subplot(2,2,4);
+imshow(lenna_smooth_k, []);
+title("Using approximation of the Wiener filter");
+
+MSEsk = mean((dlenna_r(1:M,1:N)-lenna_smooth_k).^2,'all')
+MSEs = mean((dlenna_r(1:M,1:N)-lenna_smooth).^2,'all')
+MSE = mean((dlenna_r(1:M,1:N)-lenna_gauss(1:M,1:N)).^2,'all')
