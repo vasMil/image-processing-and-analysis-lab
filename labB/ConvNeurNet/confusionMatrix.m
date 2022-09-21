198f1f8557a9767f0ed3e58950401eed223ad00c
vasMil <87028699+vasMil@users.noreply.github.com>
2022-09-21 15:14:14 +0300

    Updates confusionMatrix-differentiate (9,4) (4,9)
    There are two different cases I merged into one:
    - Having an image displaying a 9 classified as a 4, is different than
    - Having an image displaying a 4 and being classified as a 9.

--- labB/ConvNeurNet/confusionMatrix.m
+++ labB/ConvNeurNet/confusionMatrix.m
@@ -11,14 +11,14 @@ for i=1:length(predictions)
     col = predictions(i) + 1;
     row = actual_labels(i) + 1;
     confMat(row, col) = confMat(row, col) + 1;
-    if col ~= row
-        confMat(col,row) = confMat(col, row) + 1;
-    end
 end
 
-heatmap(confMat, ...
+h = heatmap(confMat, ...
     "Title", "Confusion matrix as a heatmap in logaritmic scale", ...
     "ColorScaling", "log", ...
     "XDisplayLabels", (0:9), ...
     "YDisplayLabels", (0:9));
+
+h.XLabel = "actual label";
+h.YLabel = "prediction";
 end
